ae372a70b7f023098d92724eb6428fdccae57153
Genaro Diaz <genarodiazchourio@gmail.com>
2020-07-10 13:59:29 +0200

    Updated installer for Homebrew

--- osx.sh
+++ osx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew doctor
 
