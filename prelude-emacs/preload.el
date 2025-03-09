d03b898081360d31634cc1815e28c2bc8d94d178
Genaro Díaz <genarodiazchourio@gmail.com>
2025-03-09 14:09:05 +0100

    Change default theme

---
+++ prelude-emacs/preload.el
@@ -0,0 +1,22 @@
+;;; preload.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Genaro Díaz
+;;
+;; Author: Genaro Díaz <genarodiazchourio@gmail.com>
+;; Maintainer: Genaro Díaz <genarodiazchourio@gmail.com>
+;; Created: March 09, 2025
+;; Modified: March 09, 2025
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary: Change default theme
+;;; Place here configurations that need to be done before anything else
+;;
+;;  Description
+;;
+;;; Code:
+
+(setq prelude-theme 'manoj-dark)
+;;; preload.el ends here
