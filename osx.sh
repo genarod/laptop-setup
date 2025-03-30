269af64e86f582e3d5507ed3a2f5bddf01e52730
Genaro Díaz <genarodiazchourio@gmail.com>
2025-03-30 16:23:28 +0200

    Updated with Safari config and removed Transmission

--- osx.sh
+++ osx.sh
@@ -41,7 +41,7 @@ defaults write com.apple.finder NewWindowTarget -string "PfHm"
 # Finder: Show Status bar in Finder
 defaults write com.apple.finder ShowStatusBar -bool true
 # Finder: Show Path bar in Finder
-# defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
 # echo "Show remaining battery percentage"
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 # echo "Require password 5 seconds after sleep or screen saver begins"
@@ -64,11 +64,8 @@ chflags nohidden ~/Library/
 # 10: Put display to sleep
 # 11: Launchpad
 # 12: Notification Center
-# Bottom right screen corner → Disable Screen Saver
-defaults write com.apple.dock wvous-br-corner -int 6
-defaults write com.apple.dock wvous-br-modifier -int 0
-# # Bottom left screen corner → Desktop
-defaults write com.apple.dock wvous-bl-corner -int 4
+# Bottom left screen corner → Disable Screen Saver
+defaults write com.apple.dock wvous-bl-corner -int 6
 defaults write com.apple.dock wvous-bl-modifier -int 0
 # # Top right screen corner → Start screen saver
 defaults write com.apple.dock wvous-tr-corner -int 5
@@ -83,7 +80,7 @@ defaults write com.apple.dock wvous-tr-modifier -int 0
 # defaults write com.apple.dock persistent-apps -array ""
 
 # Safari: Don't fill passwords
-defaults write com.apple.Safari AutoFillPasswords -bool false
+# defaults write com.apple.Safari AutoFillPasswords -bool false
 
 # Safari: Show developer tools
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
@@ -91,22 +88,29 @@ defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool 
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
+# Safari: Show status bar
+defaults write com.apple.Safari ShowStatusBar -bool true
+
+# Safari: Open with last session
+defaults write com.apple.Safari AlwaysRestoreSessionAtLaunch -bool true
+
+
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 # Transmission
-mkdir -p ~/Downloads/Torrents
-defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
-defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Torrents"
+# mkdir -p ~/Downloads/Torrents
+# defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+# defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Torrents"
 
 # Don't prompt for confirmation before downloading
-defaults write org.m0k.transmission DownloadAsk -bool false
+# defaults write org.m0k.transmission DownloadAsk -bool false
 
 # Hide the donate message
-defaults write org.m0k.transmission WarningDonate -bool false
+# defaults write org.m0k.transmission WarningDonate -bool false
 
 # Hide the legal disclaimer
-defaults write org.m0k.transmission WarningLegal -bool false
+# defaults write org.m0k.transmission WarningLegal -bool false
 
 # Xcode: Always use spaces for indenting
 defaults write com.apple.dt.Xcode DVTTextIndentUsingTabs -bool false
@@ -116,12 +120,11 @@ defaults write com.apple.dt.Xcode DVTTextIndentUsingTabs -bool false
 # defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
 
 for app in "Address Book" "Calendar" "Contacts" "Dashboard" "Dock" "Finder" \
-  "Mail" "Safari" "SystemUIServer" "Terminal" "Transmission" \
-  "iCal" "iTunes"; do
+  "Mail" "Safari" "SystemUIServer" "Terminal" "iCal" "iTunes"; do
   killall "$app" > /dev/null 2>&1
 done
 
 # Verify Evernote
-if [ -d "/Applications/Evernote.ap" ] then
-   osascript -e 'tell app "System Events" to display dialog "Install Evernote from App Store"'
-fi
+# if [ -d "/Applications/Evernote.ap" ] then
+#    osascript -e 'tell app "System Events" to display dialog "Install Evernote from App Store"'
+# fi
