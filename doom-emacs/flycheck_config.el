93842cc52d80d80e55523bb478d27ed618ea45f2
Genaro Díaz <genarodiazchourio@gmail.com>
2025-04-15 16:01:38 +0200

    Added flycheck config file that I am not sure is tested

---
+++ doom-emacs/flycheck_config.el
@@ -0,0 +1,23 @@
+;;; flycheck_config.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Genaro Díaz
+;;
+;; Author: Genaro Díaz <genarodiazchourio@gmail.com>
+;; Maintainer: Genaro Díaz <genarodiazchourio@gmail.com>
+;; Created: April 14, 2025
+;; Modified: April 14, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/genarodiaz/flycheck_config
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(provide 'flycheck_config)
+;;; flycheck_config.el ends here
